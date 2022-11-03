a40402368a0ac9cf6905db038c81886e2ad0fa70
Ben Kunkle <ben.kunkle@gmail.com>
2022-11-03 13:31:09 -0700

    working copy

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"os/exec"
+
 	// "io/fs"
 	"log"
 	"os"
@@ -13,15 +15,19 @@ import (
 const DB_PATH = "./db.json"
 const DB_SIZEI = 128
 
-func load_db() map[string]string {
+type DB = map[string]string
+
+// var PathNotInDbError = errors.New("Local path not found in Database")
+
+func load_db() DB {
    // TODO: return err instead of log.Fatal
-   var db map[string]string
+   var db DB
    // create db if it doesn't exist
    if _, err := os.Stat(DB_PATH); errors.Is(err, os.ErrNotExist) {
       os.Create(DB_PATH)
       if abs, err := filepath.Abs(DB_PATH); err == nil {
          fmt.Printf("creating db file: %s\n", abs)
-         db := make(map[string]string, DB_SIZEI)
+         db := make(DB, DB_SIZEI)
          return db
       } else {
          log.Fatal(err)
@@ -40,13 +46,39 @@ func load_db() map[string]string {
    return db
 }
 
-func write_db(db map[string]string) error {
+func write_db(db DB) error {
    data,err := json.MarshalIndent(db, "", "  ")
    fmt.Printf("db json: %s\n", data)
    if err == nil {
       err = os.WriteFile(DB_PATH, data, 0666)
    }
    return err
+}
+
+func copy(db DB, local_path string , from bool) error {
+   if remote_path, found := db[local_path]; !found {
+      return fmt.Errorf("local path: %s not in Databse", local_path)
+   } else {
+      source := local_path
+      dest := remote_path
+      if from {
+         source = remote_path
+         dest = local_path
+      } 
+      fmt.Printf("copying: %s -> %s\n", source, dest)
+      cmd := exec.Command("rclone", "copy", source, dest)
+      fmt.Printf("%s\n", cmd)
+      output, err := cmd.Output()
+      fmt.Println(string(output))
+      if err != nil {
+         return err
+      }
+   }
+   return nil
+}
+
+func usage() {
+   println("USAGE: goclone add local/path remote:path")
 }
 
 func main() {
@@ -57,20 +89,53 @@ func main() {
    }
 
    db := load_db()
-   if os.Args[1] == "add" {
-      if argc != 4 {
-         println("USAGE: goclone add local/path remote:path")
+   switch os.Args[1] {
+      case "add": {
+         if argc != 4 {
+            usage()
+         }
+         // TODO: check input
+         local_path, err := filepath.Abs(os.Args[2])
+         if err != nil {
+            log.Fatal(err)
+         }
+         db[local_path] = os.Args[3]
+         fmt.Printf("db: %s\n", db)
       }
-      // TODO: check input
-      local_path, err := filepath.Abs(os.Args[2])
+      case "copy": {
+         if argc > 4 {
+            usage()
+         }
+         from := false
+         local_path_idx := 2
+         if argc == 4 {
+            switch os.Args[2] {
+               case "--from", "-f": {
+                  local_path_idx = 3
+                  from = true
+               }
+               default: {
+                  switch os.Args[3] {
+                     case "--from", "-f": {
+                        from = true
+                     }
+                  }
+               }
+            }
+         }
+         local_path, err := filepath.Abs(os.Args[local_path_idx])
+
+         if err != nil {
+            log.Fatal(err)
+         }
+         err = copy(db, local_path, from)
+         if err != nil {
+            log.Fatal(err)
+         }
+         }
+      }
+      err := write_db(db)
       if err != nil {
          log.Fatal(err)
       }
-      db[local_path] = os.Args[3]
-      fmt.Printf("db: %s\n", db)
-   }
-   err := write_db(db)
-   if err != nil {
-      log.Fatal(err)
-   }
 }
