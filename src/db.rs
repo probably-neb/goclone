5a9f112ec14edcef386b3c1eea9587ad2b37a777
Ben Kunkle <ben.kunkle@gmail.com>
2022-12-12 18:32:07 -0800

    move db to module

---
+++ src/db.rs
@@ -0,0 +1,74 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::ops::{Deref, DerefMut};
+use std::collections::HashMap;
+use serde::{Serialize,Deserialize};
+
+const DB_PATH: &str = "./db";
+
+#[derive(Debug)]
+pub struct DB {
+    pub map: BiMap,
+    path_str: String,
+}
+
+pub struct Entry {
+    pub remote_path: String,
+    pub local_path: String,
+}
+
+#[derive(Deserialize,Serialize,Debug, Default)]
+pub struct BiMap {
+    pub remote_to_local: HashMap<String,String>,
+    pub local_to_remote: HashMap<String,String>,
+}
+
+impl Default for DB {
+    fn default() -> Self {
+        Self { path_str: DB_PATH.to_string(), map: Default::default() }
+    }
+}
+
+impl Deref for DB {
+    type Target = BiMap;
+
+    fn deref(&self) -> &Self::Target {
+        &self.map
+    }
+}
+
+impl DerefMut for DB {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.map
+    }
+}
+
+impl DB {
+    pub fn load() -> Self {
+        let mut db = Self::default();
+        if let Ok(mut file) = File::open(&db.path_str) {
+            let mut contents = String::new();
+            file.read_to_string(&mut contents).expect("DB Readable");
+            db.map = serde_json::from_str(&contents).expect("DB deserializable");
+        }
+        return db;
+    }
+}
+
+impl Drop for DB {
+    fn drop(&mut self) {
+        // TODO: load/store implementations
+        File::create(&self.path_str)
+            .expect("db can be opened")
+            .write_all(serde_json::to_string(&self.map)
+            .expect("DB is serializable").as_bytes())
+            .expect("DB is writeable");
+    }
+}
+
+impl BiMap {
+    pub fn insert(&mut self, entry: Entry) {
+        self.remote_to_local.insert(entry.remote_path.clone(), entry.local_path.clone());
+        self.local_to_remote.insert(entry.local_path, entry.remote_path);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use clap::{Parser,Args, Subcommand, ValueEnum};
-use serde::{Serialize,Deserialize};
-use std::{collections::hash_map::HashMap, path::Path,fs::File, error::Error, ops::{Deref, DerefMut}, io::Write};
+use clap::{Parser,Args, Subcommand};
 use enum_dispatch::enum_dispatch;
+mod db;
+use db::{DB,Entry};
 
-const DB_PATH: &str = "./db";
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -60,7 +59,7 @@ impl Add {
 
 impl Run for Add {
     fn run(&self) {
-        let mut db = DB::default();
+        let mut db = DB::load();
         db.insert(self.as_entry());
         println!("{db:?}");
     }
@@ -74,70 +73,7 @@ impl From<Add> for Entry {
     }
 }
 
-struct Entry {
-    remote_path: String,
-    local_path: String,
-}
-
-#[derive(Deserialize,Serialize,Debug, Default)]
-struct BiMap {
-    remote_to_local: HashMap<String,String>,
-    local_to_remote: HashMap<String,String>,
-}
-
-#[derive(Debug)]
-struct DB {
-    map: BiMap,
-    path_str: String,
-}
-
-impl Default for DB {
-    fn default() -> Self {
-        Self { path_str: DB_PATH.to_string(), map: Default::default() }
-    }
-}
-
-impl Deref for DB {
-    type Target = BiMap;
-
-    fn deref(&self) -> &Self::Target {
-        &self.map
-    }
-}
-
-impl DerefMut for DB {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.map
-    }
-}
-
-impl DB {
-    fn path(&self) -> &Path {
-        return Path::new(&self.path_str);
-    }
-}
-impl Drop for DB {
-    fn drop(&mut self) {
-        File::create(&self.path_str)
-            .expect("db can be opened")
-            .write_all(serde_json::to_string(&self.map)
-            .expect("DB is serializable").as_bytes())
-            .expect("DB is writeable");
-    }
-}
-
-impl BiMap {
-    pub fn insert(&mut self, entry: Entry) {
-        self.remote_to_local.insert(entry.remote_path.clone(), entry.local_path.clone());
-        self.local_to_remote.insert(entry.local_path, entry.remote_path);
-    }
-}
-
 fn main() -> std::io::Result<()> {
-    let db_path = Path::new(DB_PATH);
-    if !db_path.exists() {
-        File::create(db_path)?;
-    }
     let cli = Cli::parse();
     println!("{:?}", cli);
     cli.subcommand.run_all();
