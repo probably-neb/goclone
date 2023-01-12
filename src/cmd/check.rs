9be115cb83d4dfe7ec827b1fb60fe9dc281136de
Ben Kunkle <ben.kunkle@gmail.com>
2023-01-12 11:46:59 -0800

    fix(rclone): correctly return `Result<Stdout, Stderr>` for commands

--- src/cmd/check.rs
+++ src/cmd/check.rs
@@ -1,28 +1,27 @@
 use clap::Args;
 
 use super::Run;
-use crate::{rclone, db::Config};
+use crate::{db::Config, rclone};
 
 // TODO: add checks and --no-check flag
 #[derive(Args, Debug, Clone)]
-pub struct CheckDb {
-    // #[arg(name="remote", short, long)]
-    // remote_path: String,
-    // #[arg(name="local", short, long)]
-    // local_path: String,
-}
+pub struct CheckDb {}
 
 impl Run for CheckDb {
     fn run(&self) {
         let db = Config::load();
         println!("<local> -> <remote>");
         for entry in db.map.as_vec() {
-            let both_exist = rclone::exists(&entry.remote_path).and(rclone::exists(&entry.local_path));
+            let both_exist =
+                rclone::exists(&entry.remote_path).and(rclone::exists(&entry.local_path));
             let status = match both_exist {
                 Ok(_) => "Ok".to_string(),
-                Err(err) => format!("{:?}", err)
+                Err(err) => format!("{:?}", err),
             };
-            println!("{} -> {} ... {}", entry.local_path, entry.remote_path, status);
+            println!(
+                "{} -> {} ... {}",
+                entry.local_path, entry.remote_path, status
+            );
         }
     }
 }
--- src/rclone.rs
+++ src/rclone.rs
@@ -1,15 +1,31 @@
-use anyhow::{Context, Result};
-use std::process::{Command, Output};
+use anyhow::{Context, Result, format_err, anyhow};
+use std::process::Command;
+
+// TODO: Create rclone error type
+
+fn run(cmd: &mut Command) -> Result<String> {
+    let output = cmd.output().with_context(|| {
+        format!(
+            "cmd: `{:?} {:?}` failed to run",
+            cmd.get_program(),
+            cmd.get_args()
+        )
+    })?;
+    // return output.status.exit_ok().map(|_| str::from_utf8(&output.stdout));
+    match output.status.success() {
+        true => Ok(std::str::from_utf8(&output.stdout)?.to_string()),
+        false => Err(anyhow!(std::str::from_utf8(&output.stderr)?.to_string()))
+    }
+}
 
 fn rclone() -> Command {
     return Command::new("rclone");
 }
 
-pub fn exists(path: &String) -> Result<Output> {
-    rclone()
-        .arg("ls")
-        .arg(path)
-        .arg("--dry-run")
-        .output()
-        .with_context(|| "failed")
+pub fn exists(path: &String) -> Result<String> {
+    run(rclone().arg("lsf").arg(path).arg("--dry-run"))
+}
+
+pub fn copy(src: &str, dest: &str) -> Result<String> {
+    run(rclone().arg("copy").arg(src).arg(dest))
 }
