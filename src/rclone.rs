9955dc986be8fb6acab285767ae7618406d2d9ba
Ben Kunkle <ben.kunkle@gmail.com>
2023-01-29 10:04:18 -0800

    fix: use Command::spawn over Command::ouput for live progress

--- src/rclone.rs
+++ src/rclone.rs
@@ -1,19 +1,35 @@
 use anyhow::{anyhow, Context, Result};
-use std::process::Command;
+use std::{ffi::OsStr, process::Command};
 
 // TODO: Create rclone error type and type alias for output String
 fn run(cmd: &mut Command) -> Result<String> {
-    let output = cmd.output().with_context(|| {
-        format!(
-            "cmd: `{:?} {:?}` failed to run",
-            cmd.get_program(),
-            cmd.get_args()
-        )
-    })?;
+    cmd.arg("--progress");
+    println!(
+        "{} {}",
+        cmd.get_program().to_str().unwrap(),
+        cmd.get_args()
+            .map(OsStr::to_str)
+            .map(Option::unwrap)
+            .intersperse(" ")
+            .collect::<String>()
+    );
+    let status = cmd
+        .spawn()
+        .with_context(|| {
+            format!(
+                "cmd: `{:?} {:?}` failed to run",
+                cmd.get_program(),
+                cmd.get_args()
+            )
+        })?
+        .wait()?;
 
-    match output.status.success() {
-        true => Ok(std::str::from_utf8(&output.stdout)?.to_string()),
-        false => Err(anyhow!(std::str::from_utf8(&output.stderr)?.to_string())),
+    if status.success() {
+        // Ok(std::str::from_utf8(&output.stdout)?.to_string())
+        Ok(String::from("success"))
+    } else {
+        // Err(anyhow!(std::str::from_utf8(&output.stderr)?.to_string()))
+        Err(anyhow!("failure"))
     }
 }
 
@@ -25,7 +41,7 @@ pub fn exists(path: &str) -> Result<String> {
     run(rclone().arg("lsf").arg(path).arg("--dry-run"))
 }
 
-pub fn copy(src: &str, dest: &str, exclude: Option<impl Iterator<Item= &str>>) -> Result<String> {
+pub fn copy(src: &str, dest: &str, exclude: Option<impl Iterator<Item = &str>>) -> Result<String> {
     let mut cmd = rclone();
     cmd.arg("copy").arg(src).arg(dest);
     exclude_all(&mut cmd, exclude);
@@ -39,12 +55,20 @@ pub fn exclude(cmd: &mut Command, to_exclude: &str) {
 pub fn exclude_all(cmd: &mut Command, excluded: Option<impl Iterator<Item = &str>>) {
     if let Some(excluded) = excluded {
         for exc in excluded {
-            exclude(cmd,exc)
+            exclude(cmd, exc)
         }
     }
 }
 
-pub(crate) fn ls(path: &str, excluded: Option<impl Iterator<Item = &str>>) -> Result<String> {
+// copilot wrote this one
+pub fn sync(src: &str, dest: &str, exclude: Option<impl Iterator<Item = &str>>) -> Result<String> {
+    let mut cmd = rclone();
+    cmd.arg("sync").arg(src).arg(dest);
+    exclude_all(&mut cmd, exclude);
+    run(&mut cmd)
+}
+
+pub fn ls(path: &str, excluded: Option<impl Iterator<Item = &str>>) -> Result<String> {
     let mut cmd = rclone();
     cmd.arg("ls").arg(path);
     exclude_all(&mut cmd, excluded);
